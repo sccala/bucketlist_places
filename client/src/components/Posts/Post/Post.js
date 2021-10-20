540b5d5024f16d53298b588d03904a18fb7f4b5c
sccala <sccala18@gmail.com>
2021-10-19 17:23:16 -0700

    like post feature added

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -4,9 +4,13 @@ import DeleteIcon from '@material-ui/icons/Delete'
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
 import useStyles from './styles'
 import moment from 'moment'
+import { useDispatch } from 'react-redux'
+import { deletePost, likePost } from '../../../actions/posts'
 
 const Post = ({ post, setCurrentId }) => {
   const classes = useStyles()
+  const dispatch = useDispatch()
+
   return (
     <Card className={classes.card}>
       <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
@@ -33,10 +37,16 @@ const Post = ({ post, setCurrentId }) => {
         </Typography>
       </CardContent>
       <CardActions className={classes.cardActions}>
-        <Button size='small' color='primary' onClick={() => {}}>
+        <Button size='small' color='primary' onClick={() => dispatch(likePost(post._id))}>
           <ThumbUpAltIcon fontSize='small' /> Like {post.likeCount}
         </Button>
-        <Button size='small' color='primary' onClick={() => {}}>
+        <Button
+          size='small'
+          color='primary'
+          onClick={() => {
+            dispatch(deletePost(post._id))
+          }}
+        >
           <DeleteIcon fontSize='small' /> Delete
         </Button>
       </CardActions>
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,4 +1,4 @@
-// @ts-nocheck
+import mongoose from 'mongoose'
 import PostMessage from '../models/PostMessage.js'
 
 export const getPosts = async (req, res) => {
@@ -29,7 +29,28 @@ export const updatePost = async (req, res) => {
   const post = req.body
 
   if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id')
-  const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true })
+  const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id }, { new: true })
+  res.json(updatedPost)
+}
 
+export const deletePost = async (req, res) => {
+  const { id } = req.params
+
+  if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id')
+  await PostMessage.findByIdAndRemove(id)
+
+  res.json({ message: 'Post deleted successfully' })
+}
+
+export const likePost = async (req, res) => {
+  const { id } = req.params
+  if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id')
+
+  const post = await PostMessage.findById(id)
+  const updatedPost = await PostMessage.findByIdAndUpdate(
+    id,
+    { likeCount: post.likeCount + 1 },
+    { new: true }
+  )
   res.json(updatedPost)
 }
