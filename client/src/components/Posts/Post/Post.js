f8495b5e3b10f92d640fc50bfb03a2af5e05322e
sccala <sccala18@gmail.com>
2021-10-23 23:34:10 -0700

    fixed typo

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -13,7 +13,7 @@ const Post = ({ post, setCurrentId }) => {
 
   return (
     <Card className={classes.card}>
-      <CardMedia className={classes.media} image={post.yyyyyy} title={post.title} />
+      <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
       <div className={classes.overlay}>
         <Typography variant='h6'>{post.creator}</Typography>
         <Typography variant='body2'>{moment(post.createdAt).fromNow()}</Typography>
@@ -38,7 +38,8 @@ const Post = ({ post, setCurrentId }) => {
       </CardContent>
       <CardActions className={classes.cardActions}>
         <Button size='small' color='primary' onClick={() => dispatch(likePost(post._id))}>
-          <ThumbUpAltIcon fontSize='small' />&nbsp;Like &nbsp;{post.likeCount}
+          <ThumbUpAltIcon fontSize='small' />
+          &nbsp;Like &nbsp;{post.likeCount}
         </Button>
         <Button
           size='small'
--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,11 +1,11 @@
-import { AUTH, LOGOUT } from '../constants/actionTypes'
+import * as actionType from '../constants/actionTypes'
 
 const authReducer = (state = { authData: null }, action) => {
   switch (action.type) {
-    case AUTH:
+    case actionType.AUTH:
       localStorage.setItem('profile', JSON.stringify({ ...action?.data }))
       return { ...state, authData: action?.data }
-    case LOGOUT:
+    case actionType.LOGOUT:
       localStorage.clear()
       return { ...state, authDate: null }
     default:
