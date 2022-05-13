cff98df928b040b4ff5fd1fe4842ac8205d52967
Chase <sccala18@gmail.com>
2022-05-13 13:02:08 -0700

    added search function backend

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -30,7 +30,7 @@ const Home = () => {
   }, [currentId, dispatch])
 
   const searchPost = () => {
-    if (search.trim()) {
+    if (search.trim() || tags) {
       dispatch(getPostsBySearch({ search, tags: tags.join(',') }))
     } else {
       history.push('/')
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,19 @@
 import express from 'express'
-import { createPost, getPosts, updatePost, deletePost, likePost } from '../controllers/posts.js'
+
+import {
+  getPostsBySearch,
+  createPost,
+  getPosts,
+  updatePost,
+  deletePost,
+  likePost,
+} from '../controllers/posts.js'
 
 const router = express.Router()
 import auth from '../middleware/auth.js'
 
 router.get('/', getPosts)
+router.get('/search', getPostsBySearch)
 router.post('/', auth, createPost)
 router.patch('/:id', auth, updatePost)
 router.delete('/:id', auth, deletePost)
